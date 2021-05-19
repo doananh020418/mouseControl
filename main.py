689df11eeac291834913753021e832568c440e02
doananh020418 <65206167+doananh020418@users.noreply.github.com>
2021-05-19 18:36:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ def click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-def showCam(img,fps):
+
+def showCam(img, fps):
     # cv2.line(img, (100, 100), (100, 900), (255, 0, 0), 1)
     # cv2.line(img, (700, 100), (700, 900), (255, 0, 0), 1)
     start = (80, 80)
@@ -30,18 +31,30 @@ def showCam(img,fps):
     cv2.imshow('cam', img)
     cv2.waitKey(1)
 
-def pressArrowButton(X_left,press):
+
+def pressArrowButton(X_left, Y_left, press):
     if X_left < 80 and press:
         k.tap_key(k.right_key)
-        print('right')
+        #print('right')
         press = False
     if X_left > 880 and press:
         k.tap_key(k.left_key)
-        print('left')
+        #print('left')
         press = False
-    if X_left > 80 and X_left < 880:
+    if Y_left < 80 and press:
+        k.tap_key(k.down_key)
+        #print('down')
+        press = False
+    if Y_left > 460 and press:
+        k.tap_key(k.up_key)
+        #print('up')
+        press = False
+    if (X_left > 80 and X_left < 880) and (Y_left > 80 and Y_left < 460):
         press = True
+        #print(press)
     return press
+
+
 def main():
     plocX, plocY = 0, 0
     clocX, clocY = 0, 0
@@ -53,14 +66,14 @@ def main():
             success, img = cap.read()
             if success:
                 img = cv2.flip(img, 1)
-                print(img.shape)
+                #print(img.shape)
                 # caculate FPS
                 ctime = time.time()
                 fps = 1 / (ctime - ptime)
                 ptime = ctime
                 # get hands landmark
                 detector = HTM.handDetector(detectionCon=0.7, trackCon=0.7)
-                img, pos = detector.findHands(img, draw = True)
+                img, pos = detector.findHands(img, draw=True)
                 # get camera size
                 cw, ch, _ = img.shape
                 # get screen size
@@ -74,13 +87,11 @@ def main():
                     i, X_base, Y_base = pos[0]
 
                     # caculate distance between postiton
-                    #left_distance = math.hypot(X_left - X_base, Y_left - Y_base)
+                    # left_distance = math.hypot(X_left - X_base, Y_left - Y_base)
                     right_distance = math.hypot(X_right - X_base, Y_right - Y_base)
                     base_distance = math.hypot(X_mid - X_base, Y_mid - Y_base)
 
-
-
-                    press = pressArrowButton(X_left,press)
+                    press = pressArrowButton(X_left,Y_left, press)
                     # normalization
                     if X_left > 880:
                         X_left = 880
@@ -112,7 +123,9 @@ def main():
                     # print(f'L:{X_left, Y_left}')
                     # print(sw, sh)
 
-                print(int(fps))
-                showCam(img,fps)
+                #print(int(fps))
+                showCam(img, fps)
+
+
 if __name__ == '__main__':
     main()
